7c90c3990b2123739add92e206e6640fa0d75c70
Krishna <priyasharma6348@gmail.com>
2026-03-21 12:52:21 +0530

    updated api to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://golf-charity-platform-968z.onrender.com/api",
 });
 
 // Add token automatically
